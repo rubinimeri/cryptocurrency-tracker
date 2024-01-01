4a1d5337f4197f9e171506eea12da143727f0b35
Rubin Imeri <rubinimeri6@gmail.com>
2024-01-01 22:54:01 +0100

    Make navbar responsive and add styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css'
 import './assets/logo-dark.png'
+
+// Get DOM elements
+const container = document.querySelector(".container");
+const hamburgerMenu = document.querySelector(".hamburger");
+const bottomNav = document.querySelector(".bottom-nav")
+const topNav = document.querySelector(".top-nav")
+const searchBar = document.getElementById("search")
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    hamburgerMenu.addEventListener("click", () => {
+        bottomNav.classList.toggle("active")
+        container.classList.toggle("active")
+    })
+})
