3d89c11b6f26370b18d3134aea3ab9538f8126e9
Rubin Imeri <rubinimeri6@gmail.com>
2024-01-10 10:47:37 +0100

    Style about page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import './styles/header.css'
 import './styles/style.css'
+import './styles/about.css'
 import './assets/logo-dark.png'
-import { showMenu, moveSearchSelect, checkPage, coinCreator, addCoin } from './display-controller.js';
-import { coinWatch } from './fetch-data.js';
+import { showMenu, moveSearchSelect, checkPage, addCoin } from './display-controller.js';
+
 
 showMenu();
 moveSearchSelect();
 window.addEventListener("resize", moveSearchSelect);
 checkPage();
 
-// Add coins
-await addCoin("BTC");
-await addCoin("ETH");
-await addCoin("USDT");
+
+
+const currentPage = window.location.pathname;
+console.log(currentPage)
+
+if(currentPage === '/dist/index.html') {
+    // Add coins
+    await addCoin("BTC");
+    await addCoin("ETH");
+    await addCoin("USDT");
+}
