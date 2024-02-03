22560b926c7a7d2808c35c7667333f3e1a7e6d1c
Rubin Imeri <rubinimeri6@gmail.com>
2024-02-03 22:25:33 +0100

    Add loadChartWithData function that loads chart onto canvas

--- src/coin-chart.js
+++ src/coin-chart.js
@@ -1,1 +1,9 @@
 import Chart from 'chart.js/auto';
+
+/*
+ * Function that selects canvas from HTML and loads chart
+*/
+async function loadChartWithData(dates, prices, numTicks) {
+    const ctx = document.getElementById("myChart").getContext("2d");
+    return new Chart(ctx, config(dates, prices, numTicks));
+}
