a4a6ffa34071b436ff07254dc42513987c031f98
Rubin Imeri <rubinimeri6@gmail.com>
2024-02-03 23:15:13 +0100

    Add loadResizedChart function

--- src/coin-chart.js
+++ src/coin-chart.js
@@ -1,60 +1,74 @@
-import Chart from 'chart.js/auto';
+import Chart from "chart.js/auto";
 
 /*
  * Function that selects canvas from HTML and loads chart
-*/
+ */
 export async function loadChartWithData(dates, prices, numTicks) {
-    const ctx = document.getElementById("myChart").getContext("2d");
-    return new Chart(ctx, config(dates, prices, numTicks));
+  const ctx = document.getElementById("myChart").getContext("2d");
+  return new Chart(ctx, config(dates, prices, numTicks));
 }
 
 /*
  * Configuration for the chart
-*/
+ */
 const config = function (dates, prices, numTicks) {
-    return {
-        type: "line",
-        data: {
-            labels: dates,
-            datasets: [
-                {
-                    color: "rgb(0,0,0)",
-                    data: prices,
-                    borderWidth: 2,
-                    pointRadius: 0,
-                    pointHitRadius: 10,
-                    pointHoverRadius: 0,
-                }
-            ],
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                layout: {
-                    padding: {
-                        left: 10,
-                    }
-                },
-                scales: {
-                    x: {
-                        grid: {
-                            display: false
-                        },
-                        ticks: {
-                            maxTicksLimit: numTicks,
-                            align: "start",
-                            color: "gray",
-                            font: {
-                                weight: "bold"
-                            }
-                        }
-                    }
-                },
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                }
-            }
+  return {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          color: "rgb(0,0,0)",
+          data: prices,
+          borderWidth: 2,
+          pointRadius: 0,
+          pointHitRadius: 10,
+          pointHoverRadius: 0,
         },
+      ],
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        layout: {
+          padding: {
+            left: 10,
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false,
+            },
+            ticks: {
+              maxTicksLimit: numTicks,
+              align: "start",
+              color: "gray",
+              font: {
+                weight: "bold",
+              },
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    },
+  };
+};
+
+/*
+ * Recreate the chart with updated properties depending on
+ * window width
+*/
+function loadResizedChart(dates, prices) {
+    if(window.innerWidth <= 900 && window.innerWidth > 640) {
+      loadChartWithData(dates, prices, 7);
     }
-}
+    else if(window.innerWidth <= 640) {
+      loadChartWithData(dates, prices, 4);
+    }
+    else loadChartWithData(dates, prices, 10);
+};
