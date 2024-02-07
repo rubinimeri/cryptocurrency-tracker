532d31c4d8c7aaec1cf90e0c0d413a89b69bdcb6
Rubin Imeri <rubinimeri6@gmail.com>
2024-02-07 19:40:37 +0100

    Add coinCounter to order coins

--- src/coin-data.js
+++ src/coin-data.js
@@ -1,6 +1,14 @@
+// * Create counter for Coin objects
+let coinCounter = 1;
+
 // Create Coin objects
 
 export function Coin(rank, logo, name, price, hour, day, marketCap, volume, circulatingSupply) {
+    rank = coinCounter;
+
+    // * Increment counter
+    coinCounter++;
+
     // Adjust data
     price = price.toFixed(2);
     hour = ((hour - 1) * 100).toFixed(2);
