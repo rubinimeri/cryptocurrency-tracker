6dca80bf9ff4e5546d209ad20ecf6a21bc28a7bd
Rubin Imeri <rubinimeri6@gmail.com>
2024-02-09 18:59:40 +0100

    Remove babel and html webpack plugin due to errors

--- webpack.config.js
+++ webpack.config.js
@@ -18,16 +18,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            // Options for Babel loader (usually from .babelrc)
-          }
-        }
-      }
     ],
   },
 };
