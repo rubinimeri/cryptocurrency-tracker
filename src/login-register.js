bc277b2eb5432a2562c3059245ae9deb968545c7
Rubin Imeri <rubinimeri6@gmail.com>
2024-03-15 13:31:12 +0100

    Refactor login-register.s file

--- src/login-register.js
+++ src/login-register.js
@@ -1,6 +1,10 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { getDatabase, ref, set } from 'firebase/database';
+import {
+  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,
+} from 'firebase/auth';
+import {
+  getDatabase, ref, set, get,
+} from 'firebase/database';
 
 export default function setRegister() {
   const loginHeader = document.querySelector('.login');
@@ -16,31 +20,6 @@ export default function setRegister() {
 
 const loginForm = document.querySelector('.login-form');
 const registerForm = document.querySelector('.register-form');
-
-export function loadAuth() {
-  if (localStorage.getItem('register') === 'true') {
-    loginForm.classList.add('display-none');
-    registerForm.classList.remove('display-none');
-  } else {
-    loginForm.classList.remove('display-none');
-    registerForm.classList.add('display-none');
-  }
-}
-
-export function changeAuth() {
-  const loginSpan = document.querySelector('.login-span');
-  const registerSpan = document.querySelector('.register-span');
-
-  loginSpan.addEventListener('click', () => {
-    loginForm.classList.remove('display-none');
-    registerForm.classList.add('display-none');
-  });
-
-  registerSpan.addEventListener('click', () => {
-    loginForm.classList.add('display-none');
-    registerForm.classList.remove('display-none');
-  });
-}
 
 // Validate Functions
 function validateEmail(email) {
@@ -83,82 +62,120 @@ export function initApp() {
     messagingSenderId: '574850829297',
     appId: '1:574850829297:web:eda4dcdc57384f4b155250',
   };
+  const page = window.location.pathname;
 
   // Initialize firebase
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const database = getDatabase(app);
 
-  const registerButton = document.getElementById('register');
-  const loginButton = document.getElementById('login');
+  function getUserDataFromDatabase(userId) {
+    const userReference = ref(database, `users/${userId}`);
+    return get(userReference)
+      .then((userSnapshot) => {
+        if (userSnapshot.exists()) {
+          return userSnapshot.val();
+        }
+      })
+      .catch(console.error);
+  }
 
-  // Add event listener to register form
-  registerButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    // Get input values
-    const fullName = document.querySelector('.full-name').value;
-    const username = document.querySelector('.username').value;
-    const email = registerForm.querySelector('.email-form').value;
-    const password = registerForm.querySelector('.password-form').value;
-
-    // Validate email and password
-    if (validateEmail(email) === false || validatePassword(password) === false) {
-      alert('Email or password format is incorrect');
-      return;
-    }
-
-    // Validate full name and username
-    if (validateField(username) === false || validateField(fullName) === false) {
-      alert('Username and fullname must not be empty');
-      return;
-    }
-
-    // Create user
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Handle successful registration
-        const { user } = userCredential;
-        console.log(user);
-
-        // Store additional user data in the database
-        const userId = user.uid;
-        const userRef = ref(database, `users/${userId}`);
-        set(userRef, {
-          fullName,
-          username,
-          email,
-          lastLogin: Date.now(),
+  // Check the user's authentication state
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in
+      console.log('User is signed in:', user);
+      const userId = user.uid;
+      getUserDataFromDatabase(userId)
+        .then((userData) => {
+          if (userData) {
+            console.log("User's data:", userData.email, userData.fullName, userData.username);
+          } else {
+            console.log("User's data not found in the database");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching user's data:", error);
         });
-      })
-      .catch((error) => {
-        // Handle registration error
-        console.error('Registration error:', error.message);
-      });
-  });
-
-  loginButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    // Get input values
-    const email = loginForm.querySelector('.email-form').value;
-    const password = loginForm.querySelector('.password-form').value;
-
-    // Validate email and password
-    if (validateEmail(email) === false || validatePassword(password) === false) {
-      alert('Email or password format is incorrect');
-      return;
+    } else {
+      // User is signed out
+      console.log('User is signed out');
     }
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-      // Handle successful login
-        const { user } = userCredential;
-        console.log('User logged in:', user);
-      })
-      .catch((error) => {
-      // Handle login error
-        console.error('Login error:', error.message);
-      });
   });
+
+  if (page.includes('login.html')) {
+    const loginButton = document.getElementById('login');
+    loginButton.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      // Get input values
+      const email = loginForm.querySelector('.email-form').value;
+      const password = loginForm.querySelector('.password-form').value;
+
+      // Validate email and password
+      if (validateEmail(email) === false || validatePassword(password) === false) {
+        alert('Email or password format is incorrect');
+        return;
+      }
+
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        // Handle successful login
+          const { user } = userCredential;
+          console.log('User logged in:', user);
+        })
+        .catch((error) => {
+        // Handle login error
+          console.error('Login error:', error.message);
+        });
+    });
+  }
+
+  if (page.includes('register.html')) {
+    const registerButton = document.getElementById('register');
+    // Add event listener to register form
+    registerButton.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      // Get input values
+      const fullName = document.querySelector('.full-name').value;
+      const username = document.querySelector('.username').value;
+      const email = registerForm.querySelector('.email-form').value;
+      const password = registerForm.querySelector('.password-form').value;
+
+      // Validate email and password
+      if (validateEmail(email) === false || validatePassword(password) === false) {
+        alert('Email or password format is incorrect');
+        return;
+      }
+
+      // Validate full name and username
+      if (validateField(username) === false || validateField(fullName) === false) {
+        alert('Username and fullname must not be empty');
+        return;
+      }
+
+      // Create user
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        // Handle successful registration
+          const { user } = userCredential;
+          console.log(user);
+
+          // Store additional user data in the database
+          const userId = user.uid;
+          const userRef = ref(database, `users/${userId}`);
+          set(userRef, {
+            fullName,
+            username,
+            email,
+            lastLogin: Date.now(),
+          });
+        })
+        .catch((error) => {
+        // Handle registration error
+          console.error('Registration error:', error.message);
+        });
+    });
+  }
 }
